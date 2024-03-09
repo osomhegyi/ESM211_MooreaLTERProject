aa070d3168d7dda4b168ceef5d7474d50a3be208
meghanroberts <meghanroberts@bren.ucsb.edu>
2024-03-09 13:30:10 -0800

    all done

--- GLARMA_ESM211.R
+++ GLARMA_ESM211.R
@@ -1,11 +1,11 @@
-# GLARMA example
+# GLARMA Moorea Parrotfish
 # March 2024
-# Modified by Tim Commerford, Zack de la Rocha, Tom Morello and Brad Wilk with contributions from CLJ
 ################
 
 library(glarma)
 library(here) # makes a local directory for the R project
 library(tidyverse)
+library(gridExtra)
 
 #############################################load in that data#############################
 PF1<-read.csv(here("data","common_parrotfish_data.csv"))
@@ -120,38 +120,44 @@ model_business <- function(PF, site_number) {
   coefficients_df$site<-site_number
   
   # return(result_df)
-  return(list(result_df, coefficients_df))
+  return(list(result_df, coefficients_df, glarmamod.7B.PF))
 }
 
 #outputs for site 1
-site1_results_list<-model_business(PF1, 1)
-site1_results<- site1_results_list[[1]]  
-site1_coefficients_df <- site1_results_list[[2]] 
+site1_results_list<-model_business(PF1, 1) 
+site1_results<- site1_results_list[[1]]  #for choosing model
+site1_coefficients_df <- site1_results_list[[2]]  #for coefficient plotting
+site1_7B_output<-site1_results_list[[3]] #for plot model vs data
 
 #outputs for site 2
 site2_results_list<-model_business(PF2, 2)
 site2_results<- site2_results_list[[1]]  
 site2_coefficients_df <- site2_results_list[[2]] 
+site2_7B_output<-site2_results_list[[3]] 
 
 #outputs for site 3
 site3_results_list<-model_business(PF3, 3)
 site3_results<- site3_results_list[[1]]  
-site3_coefficients_df <- site3_results_list[[2]] 
+site3_coefficients_df <- site3_results_list[[2]]
+site3_7B_output<-site3_results_list[[3]] 
 
 #outputs for site 4
 site4_results_list<-model_business(PF4, 4)
 site4_results<- site4_results_list[[1]]  
 site4_coefficients_df <- site4_results_list[[2]] 
+site4_7B_output<-site4_results_list[[3]] 
 
 #outputs for site 5
 site5_results_list<-model_business(PF5, 5)
 site5_results<- site5_results_list[[1]]  
-site5_coefficients_df <- site5_results_list[[2]] 
+site5_coefficients_df <- site5_results_list[[2]]
+site5_7B_output<-site5_results_list[[3]] 
 
 #outputs for site 6
 site6_results_list<-model_business(PF6, 6)
 site6_results<- site6_results_list[[1]]  
 site6_coefficients_df <- site6_results_list[[2]] 
+site6_7B_output<-site6_results_list[[3]] 
 
 
 ##################################keep only the best models############################
@@ -260,112 +266,147 @@ ggsave(here("figures","cyclone_coefficients.jpg"), cyclone_plot, width=8, height
 
 ############################make plots of 7B fit for each site###########################
 
+plot_7B_fun<-function(raw_data, model_output, title_site_string){
+  
+  PF_mod<-raw_data
+  
+  PF_mod$est<-model_output$fitted.values #adds the fitted values of the GLARMA model to the data
+  
+  #plots the model (black) and the species richness data (gray)
+  PF_plot_mod<-ggplot(PF_mod, aes(x=year)) + 
+    geom_point(col="gray",aes(y=annual_count),size=3) + 
+    geom_point(col="black",aes(y=est),size=3) +
+    geom_line(col="black",aes(y=est)) +
+    labs(x="Year", y="Parrot Fish Count", title=title_site_string) +
+    ylim(0,400) + xlim(2005,2023)+
+    theme_bw()
+  
+  return(PF_plot_mod)
+  
+}
+
+Site1_Model7B<-plot_7B_fun(PF1, site1_7B_output, "Site 1")
+ggsave(here("figures","Site1_Model7B.jpg"),Site1_Model7B, dpi=500, width=6, height=5, unit="in")
+
+Site2_Model7B<-plot_7B_fun(PF2, site2_7B_output, "Site 2")
+ggsave(here("figures","Site2_Model7B.jpg"),Site2_Model7B, dpi=500, width=6, height=5, unit="in")
+
+Site3_Model7B<-plot_7B_fun(PF3, site3_7B_output, "Site 3")
+ggsave(here("figures","Site3_Model7B.jpg"),Site3_Model7B, dpi=500, width=6, height=5, unit="in")
+
+Site4_Model7B<-plot_7B_fun(PF4, site4_7B_output, "Site 4")
+ggsave(here("figures","Site4_Model7B.jpg"),Site4_Model7B, dpi=500, width=6, height=5, unit="in")
+
+Site5_Model7B<-plot_7B_fun(PF5, site5_7B_output, "Site 5")
+ggsave(here("figures","Site5_Model7B.jpg"),Site5_Model7B, dpi=500, width=6, height=5, unit="in")
+
+Site6_Model7B<-plot_7B_fun(PF6, site6_7B_output, "Site 6")
+ggsave(here("figures","Site6_Model7B.jpg"),Site6_Model7B, dpi=500, width=6, height=5, unit="in")
+
+all_7B_plots<-grid.arrange(Site1_Model7B, Site2_Model7B, Site3_Model7B,
+               Site4_Model7B, Site5_Model7B, Site6_Model7B,
+               nrow = 2, ncol = 3)
+ggsave(here("figures","all_Model7B.jpg"),all_7B_plots, dpi=500, width=12, height=8, unit="in")
 
 
-
-
-
-
-
-
-
-
-#Manually
-############################################
-#Change: the intercept should be 1 not 0
-PF<-read.csv(here("data","common_parrotfish_data.csv"))
-PF$intercept<-1
-
-############################################
-
-# Y.SR<-SR$S # makes the response variable only the species richness (constant across models)
-Y.PF<-PF$annual_count
-
-# X.1.SR<-SR %>% select(intercept, dist) #makes model 1's explanatory variables of the intercept (1)
-X.1.PF<-PF |> select(intercept, major_event_crown_of_thorns, major_event_bleaching, major_event_cylcone) # makes a model with crown of thorns event and intercept (1)
-X.2.PF<-PF |> select(intercept, major_event_bleaching) # makes a model with crown of thorns event and intercept (1)
-
-#Makes into a matrix
-# X.1.SR<-as.matrix(X.1.SR) 
-X.1.PF<-as.matrix(X.1.PF)
-X.2.PF<-as.matrix(X.2.PF)
-
-#SR model 1
-# model with fire events included
-# glarmamod.1.SR <- glarma(Y.SR, X.1.SR, phiLags = c(1), type = "Poi", method = "FS",
-                      # residuals = "Pearson", maxit = 100, grad = 1e-6)
-glarmamod.1A.PF <- glarma(Y.PF, X.1.PF, phiLags = c(1), type = "Poi", method = "FS",
-                         residuals = "Pearson", maxit = 100, grad = 1e-6)
-glarmamod.1B.PF <- glarma(Y.PF, X.1.PF, phiLags = c(2), type = "Poi", method = "FS",
-                         residuals = "Pearson", maxit = 100, grad = 1e-6)
-glarmamod.2A.PF <- glarma(Y.PF, X.2.PF, phiLags = c(1), type = "Poi", method = "FS",
-                          residuals = "Pearson", maxit = 100, grad = 1e-6)
-glarmamod.2B.PF <- glarma(Y.PF, X.2.PF, phiLags = c(2), type = "Poi", method = "FS",
-                          residuals = "Pearson", maxit = 100, grad = 1e-6)
-
-
-#summary statistics just like getting information out of linear regression
-# summary(glarmamod.1.SR)
-glarmamod.1A.PF.summary<-summary(glarmamod.1A.PF)
-glarmamod.1A.PF
-beta_coefficients <- glarmamod.1A.PF.summary$coefficients1
-
-
-glarmamod.1B.PF.summary<-summary(glarmamod.1B.PF)
-
-glarmamod.2A.PF.summary<-summary(glarmamod.2A.PF)
-
-glarmamod.2B.PF.summary<-summary(glarmamod.2B.PF)
-
-#### Modified PF plot for Moorea
-PF_mod<-PF
-
-PF_mod$est<-glarmamod.1B.PF$fitted.values #adds the fitted values of the GLARMA model to the data
-
-#plots the model (black) and the species richness data (gray)
-PF_plot_mod<-ggplot(PF_mod, aes(x=year)) + 
-  geom_point(col="gray",aes(y=annual_count),size=3) + 
-  geom_point(col="black",aes(y=est),size=3) +
-  geom_line(col="black",aes(y=est)) +
-  labs(x="Year", y="Parrot Fish Count") +
-  ylim(0,400) + xlim(2005,2023)+
-  theme_bw()
-
-PF_plot_mod
-ggsave(here("plot","Moorea_PF_Glarma_m1.jpg"),PF_plot_mod, dpi=500, width=6, height=3, unit="in")
-
-
-#site 2 model 4 figure
-PF_mod<-PF2
-
-PF_mod$est<-glarmamod.4.PF2$fitted.values #adds the fitted values of the GLARMA model to the data
-
-#plots the model (black) and the species richness data (gray)
-PF_plot_mod<-ggplot(PF_mod, aes(x=year)) + 
-  geom_point(col="gray",aes(y=annual_count),size=3) + 
-  geom_point(col="black",aes(y=est),size=3) +
-  geom_line(col="black",aes(y=est)) +
-  labs(x="Year", y="Parrot Fish Count") +
-  ylim(0,400) + xlim(2005,2023)+
-  theme_bw()
-
-PF_plot_mod
-
-#site 3 model 4 figure
-PF_mod<-PF3
-
-PF_mod$est<-glarmamod.4.PF3$fitted.values #adds the fitted values of the GLARMA model to the data
-
-#plots the model (black) and the species richness data (gray)
-PF_plot_mod<-ggplot(PF_mod, aes(x=year)) + 
-  geom_point(col="gray",aes(y=annual_count),size=3) + 
-  geom_point(col="black",aes(y=est),size=3) +
-  geom_line(col="black",aes(y=est)) +
-  labs(x="Year", y="Parrot Fish Count") +
-  ylim(0,400) + xlim(2005,2023)+
-  theme_bw()
-
-PF_plot_mod
+# 
+# 
+# #Manually
+# ############################################
+# #Change: the intercept should be 1 not 0
+# PF<-read.csv(here("data","common_parrotfish_data.csv"))
+# PF$intercept<-1
+# 
+# ############################################
+# 
+# # Y.SR<-SR$S # makes the response variable only the species richness (constant across models)
+# Y.PF<-PF$annual_count
+# 
+# # X.1.SR<-SR %>% select(intercept, dist) #makes model 1's explanatory variables of the intercept (1)
+# X.1.PF<-PF |> select(intercept, major_event_crown_of_thorns, major_event_bleaching, major_event_cylcone) # makes a model with crown of thorns event and intercept (1)
+# X.2.PF<-PF |> select(intercept, major_event_bleaching) # makes a model with crown of thorns event and intercept (1)
+# 
+# #Makes into a matrix
+# # X.1.SR<-as.matrix(X.1.SR) 
+# X.1.PF<-as.matrix(X.1.PF)
+# X.2.PF<-as.matrix(X.2.PF)
+# 
+# #SR model 1
+# # model with fire events included
+# # glarmamod.1.SR <- glarma(Y.SR, X.1.SR, phiLags = c(1), type = "Poi", method = "FS",
+#                       # residuals = "Pearson", maxit = 100, grad = 1e-6)
+# glarmamod.1A.PF <- glarma(Y.PF, X.1.PF, phiLags = c(1), type = "Poi", method = "FS",
+#                          residuals = "Pearson", maxit = 100, grad = 1e-6)
+# glarmamod.1B.PF <- glarma(Y.PF, X.1.PF, phiLags = c(2), type = "Poi", method = "FS",
+#                          residuals = "Pearson", maxit = 100, grad = 1e-6)
+# glarmamod.2A.PF <- glarma(Y.PF, X.2.PF, phiLags = c(1), type = "Poi", method = "FS",
+#                           residuals = "Pearson", maxit = 100, grad = 1e-6)
+# glarmamod.2B.PF <- glarma(Y.PF, X.2.PF, phiLags = c(2), type = "Poi", method = "FS",
+#                           residuals = "Pearson", maxit = 100, grad = 1e-6)
+# 
+# 
+# #summary statistics just like getting information out of linear regression
+# # summary(glarmamod.1.SR)
+# glarmamod.1A.PF.summary<-summary(glarmamod.1A.PF)
+# glarmamod.1A.PF
+# beta_coefficients <- glarmamod.1A.PF.summary$coefficients1
+# 
+# 
+# glarmamod.1B.PF.summary<-summary(glarmamod.1B.PF)
+# 
+# glarmamod.2A.PF.summary<-summary(glarmamod.2A.PF)
+# 
+# glarmamod.2B.PF.summary<-summary(glarmamod.2B.PF)
+# 
+# #### Modified PF plot for Moorea
+# PF_mod<-PF
+# 
+# PF_mod$est<-glarmamod.1B.PF$fitted.values #adds the fitted values of the GLARMA model to the data
+# 
+# #plots the model (black) and the species richness data (gray)
+# PF_plot_mod<-ggplot(PF_mod, aes(x=year)) + 
+#   geom_point(col="gray",aes(y=annual_count),size=3) + 
+#   geom_point(col="black",aes(y=est),size=3) +
+#   geom_line(col="black",aes(y=est)) +
+#   labs(x="Year", y="Parrot Fish Count") +
+#   ylim(0,400) + xlim(2005,2023)+
+#   theme_bw()
+# 
+# PF_plot_mod
+# ggsave(here("plot","Moorea_PF_Glarma_m1.jpg"),PF_plot_mod, dpi=500, width=6, height=3, unit="in")
+# 
+# 
+# #site 2 model 4 figure
+# PF_mod<-PF2
+# 
+# PF_mod$est<-glarmamod.4.PF2$fitted.values #adds the fitted values of the GLARMA model to the data
+# 
+# #plots the model (black) and the species richness data (gray)
+# PF_plot_mod<-ggplot(PF_mod, aes(x=year)) + 
+#   geom_point(col="gray",aes(y=annual_count),size=3) + 
+#   geom_point(col="black",aes(y=est),size=3) +
+#   geom_line(col="black",aes(y=est)) +
+#   labs(x="Year", y="Parrot Fish Count") +
+#   ylim(0,400) + xlim(2005,2023)+
+#   theme_bw()
+# 
+# PF_plot_mod
+# 
+# #site 3 model 4 figure
+# PF_mod<-PF3
+# 
+# PF_mod$est<-glarmamod.4.PF3$fitted.values #adds the fitted values of the GLARMA model to the data
+# 
+# #plots the model (black) and the species richness data (gray)
+# PF_plot_mod<-ggplot(PF_mod, aes(x=year)) + 
+#   geom_point(col="gray",aes(y=annual_count),size=3) + 
+#   geom_point(col="black",aes(y=est),size=3) +
+#   geom_line(col="black",aes(y=est)) +
+#   labs(x="Year", y="Parrot Fish Count") +
+#   ylim(0,400) + xlim(2005,2023)+
+#   theme_bw()
+# 
+# PF_plot_mod
 
 
 
